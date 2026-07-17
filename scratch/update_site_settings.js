59d9e156cca083d155f0fbbd267c8aa46811f875
rimurudesing <rimurudesing@github.com>
2026-07-17 03:36:12 -0400

    build: update download URL to final 10MB MediaFire link in database helper script

--- scratch/update_site_settings.js
+++ scratch/update_site_settings.js
@@ -15,7 +15,7 @@ async function main() {
       latest_version: "1.3.2",
       latest_changelog: "- Arreglo de clics y gestos táctiles en reproductor y menús\n- Mensajes de voz (audios) habilitados en el chat\n- Rediseño responsivo de estadísticas e historial en perfil\n- Optimización de tamaño de APK de 768MB a 15MB",
       global_ads_enabled: true,
-      latest_download_url: "https://www.mediafire.com/file/1xhsh8vi2qo9ve8/Cineverse.apk/file"
+      latest_download_url: "https://www.mediafire.com/file/mmutp4p5facd7lr/Cineverse.apk/file"
     };
 
     const query = "UPDATE public.site_settings SET value = $1 WHERE key = 'global_config' RETURNING *;";
